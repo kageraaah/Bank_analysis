2efe25ab30125ad01e9065f393904c6487157ddf
tylexx12 <tyler.maina@strathmore.edu>
2025-11-24 12:16:55 +0300

    some major changes to the gui

--- src/TestChart.java
+++ src/TestChart.java
@@ -6,18 +6,19 @@ import javax.swing.*;
 
 public class TestChart {
     public static void main(String[] args) {
+        // Create dataset
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         dataset.addValue(1, "Series1", "A");
         dataset.addValue(4, "Series1", "B");
         dataset.addValue(3, "Series1", "C");
-
+        // Create bar chart
         JFreeChart chart = ChartFactory.createBarChart(
                 "Test Chart",
                 "Category",
                 "Value",
                 dataset
         );
-
+        // Display chart in JFrame
         JFrame frame = new JFrame("Chart Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(new ChartPanel(chart));
